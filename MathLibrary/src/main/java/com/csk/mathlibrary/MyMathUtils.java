488feee0aa52274bf0d45f08d7734e30b70719df
praveen <praveen@avaniinfosoft.co.in>
2024-01-25 15:55:51 +0530

    test

---
+++ MathLibrary/src/main/java/com/csk/mathlibrary/MyMathUtils.java
@@ -0,0 +1,10 @@
+package com.csk.mathlibrary;
+
+import android.util.Log;
+
+public class MyMathUtils {
+    public static void add(int x, int y) {
+        int res = x + y;
+        Log.d("add", res + "");
+    }
+}
